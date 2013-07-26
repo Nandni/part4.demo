37da90346606d195b95a7d4ab8f8ccfc7b65b1f6
Nandni <nandni@xemesolutions.com>
2013-07-26 12:19:28 +0530

    add index2.php

---
+++ index2.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<?php
+echo '<table width="50%" border="1" cellspacing="10" cellpadding="10">
+  <caption align="top">
+    Table
+  </caption>
+  <tr>
+    <td>Abc</td>
+    <td>def</td>
+    <td>fds</td>
+  </tr>
+  <tr>
+    <td>sdfd</td>
+    <td>fgh</td>
+    <td>gfjh</td>
+  </tr>
+  <tr>
+    <td>fgj</td>
+    <td>fgjh</td>
+    <td>fgj</td>
+  </tr>
+</table>';
+?>
+</body>
+</html>
